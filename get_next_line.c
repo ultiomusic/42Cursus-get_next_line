a3640414c381789453b0fdc3cca3eb915ec98a47
Ultio <94832543+ultiomusic@users.noreply.github.com>
2023-05-13 01:38:55 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,31 +1,5 @@
 #include "get_next_line.h"
 
-char	*next_read(char *str)
-{
-	int		i;
-	int		j;
-	char	*next_str;
-
-	i = 0;
-	while (str[i] != '\n' && str[i])
-		i++;
-	if (str[i] == 0)
-	{
-		free(str);
-		return (NULL);
-	}
-	next_str = (char *)malloc(sizeof(char) * (ft_strlen(str) - i));
-	if (!next_str)
-		return (NULL);
-	i++;
-	j = 0;
-	while (str[i])
-		next_str[j++] = str[i++];
-	next_str[j] = 0;
-	free(str);
-	return (next_str);
-}
-
 char	*read_line(char *str, int fd)
 {
 	char	*line;
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,56 +1,5 @@
 #include "get_next_line_bonus.h"
 
-char	*next_read(char *str)
-{
-	int		i;
-	int		j;
-	char	*next_str;
-
-	i = 0;
-	while (str[i] != '\n' && str[i])
-		i++;
-	if (str[i] == 0)
-	{
-		free(str);
-		return (NULL);
-	}
-	next_str = (char *)malloc(sizeof(char) * (ft_strlen(str) - i));
-	if (!next_str)
-		return (NULL);
-	i++;
-	j = 0;
-	while (str[i])
-		next_str[j++] = str[i++];
-	next_str[j] = 0;
-	free(str);
-	return (next_str);
-}
-
-char	*read_first_line(char *str)
-{
-	char	*new_str;
-	int		i;
-
-	i = 0;
-	if (!str[i])
-		return (NULL);
-	while (str[i] != '\n' && str[i])
-		i++;
-	new_str = malloc(sizeof(char) * (i + 2));
-	if (!new_str)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\n' && str[i])
-	{
-		new_str[i] = str[i];
-		i++;
-	}
-	if (str[i] == '\n')
-		new_str[i++] = '\n';
-	new_str[i] = 0;
-	return (new_str);
-}
-
 char	*read_line(char *str, int fd)
 {
 	char	*line;
